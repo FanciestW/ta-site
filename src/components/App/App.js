a5607fd1376961ce8968a505d3016636aa24eacc
William Lin <wlin26@yahoo.com>
2019-10-01 22:39:01 -0400

    Added material ui and app bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Calendar from '../Calendar/Calendar';
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class App extends Component {
 
   render() {
     return (
       <div className='app'>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start"color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
         <Calendar />
       </div>
     );
