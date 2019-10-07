55cba34b8ba665010942f03b0adc0ccc96563477
William Lin <wlin26@yahoo.com>
2019-10-06 22:48:55 -0400

    Completed contact info page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Navbar from '../Navbar/Navbar';
 import Calendar from '../Calendar/Calendar';
 import Contact from '../Contact/Contact';
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardActions, CardContent, Divider, Grid, Button, Typography, CardMedia } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, Divider, Grid, Link, Typography, CardMedia } from '@material-ui/core';
+import EmailIcon from '@material-ui/icons/Email';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import profile_pic from './profile_pic.jpg';
 import './Contact.scss';
 
 class Contact extends Component {
@@ -11,28 +14,33 @@ class Contact extends Component {
         direction="column"
         alignItems="center"
         justify="center"
-        style={{ minHeight: '100vh' }}>
+        style={{ minHeight: '90vh' }}>
         <Card className={'contact-card'}>
+          <CardMedia component="img"
+            alt="Picture"
+            height="250"
+            image={profile_pic}
+            title="Profile Picture"/>
           <CardContent>
-            <CardMedia component="img"
-              alt="Picture"
-              height="200"
-              image="./logo192.png"
-              title="Profile Picture">
-
-            </CardMedia>
             <Typography variant="h3" component="p" align="left">
               William Lin
             </Typography>
-            <Typography variant="caption" component="p" align="left">
+            <Typography variant="subtitle1" component="p" align="left">
               Computer Science Teaching Assistant
             </Typography>
-            <Divider></Divider>
-            
-            <Typography variant="body1" component="p" align="left">
-              wlin2@unh.newhaven.edu
+            <Divider className="card-divider"/>
+            <Typography variant="body1" component="p" align="left" className="contact-info">
+              <EmailIcon className="icons"/>
+              <Link href="mailto:wlin2@unh.newhaven.edu" color="inherit" className="contact-link">
+                wlin2@unh.newhaven.edu
+              </Link>
             </Typography>
-
+            <Typography variant="body1" component="p" align="left" className="contact-info">
+              <PhoneIphoneIcon className="icons"/>
+              <Link href="sms:917-444-5340" color="inherit" className="contact-link">
+                (917) 444-5340
+              </Link>
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
