a535f1b6dd00add26ec977a631fc2d91e78ef34c
William Lin <wlin26@yahoo.com>
2019-10-06 23:05:58 -0400

    Added classes comonent to replace resources

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Calendar from '../Calendar/Calendar';
+import Classes from '../Classes/Classes';
 import Contact from '../Contact/Contact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import TodayIcon from '@material-ui/icons/Today';
 import HomeIcon from '@material-ui/icons/Home';
-import ComputerIcon from '@material-ui/icons/Computer';
+import SchoolIcon from '@material-ui/icons/School';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
 import './App.scss';
 require('dotenv').config();
@@ -21,19 +22,6 @@ function RouteHome() {
   return <h2>Home</h2>;
 }
 
-function RouteCalendar() {
-  return <Calendar />;
-}
-
-function RouteResource() {
-  return <h2>Resource</h2>;
-}
-
-function RouteContact() {
-  return <Contact />;
-}
-
-
 class App extends Component {
 
   constructor(props) {
@@ -41,25 +29,21 @@ class App extends Component {
   }
 
   render() {
-    const navTitles = ['Home', 'Calendar', 'Resources', 'Contact'];
-    const navLinks = ['/', '/calendar', '/resources', '/contact'];
-    const navIcons = [<HomeIcon key={0}/>, <TodayIcon key={1}/>, <ComputerIcon key={3}/>, <ContactMailIcon key={4}/>];
+    const navTitles = ['Home', 'Calendar', 'Classes', 'Contact'];
+    const navLinks = ['/', '/calendar', '/classes', '/contact'];
+    const navIcons = [<HomeIcon key={0} />, <TodayIcon key={1} />, <SchoolIcon key={3} />, <ContactMailIcon key={4} />];
     return (
       <MuiThemeProvider theme={theme}>
         <Router>
           <div className='app'>
-            <Navbar titles={navTitles} links={navLinks} icons={navIcons} onClick={this.test}/>
+            <Navbar titles={navTitles} links={navLinks} icons={navIcons} onClick={this.test} />
             <Switch>
               <Route exact path='/'>
                 <RouteHome />
               </Route>
               <Route exact path='/calendar' component={Calendar} />
-              <Route exact path='/resources'>
-                <RouteResource />
-              </Route>
-              <Route exact path='/contact'>
-                <RouteContact />
-              </Route>
+              <Route exact path='/classes' component={Classes} />
+              <Route exact path='/contact' component={Contact} />
             </Switch>
           </div>
         </Router>
---
+++ src/components/Classes/Classes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Classes.scss';
+
+class Classes extends Component {
+  render() {
+    return (
+      <h1>Classes</h1>
+    );
+  }
+}
+
+export default Classes;
