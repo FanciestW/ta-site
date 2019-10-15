489377a76f80b3ceee704a2fc4e36991a1e57e39
William Lin <wlin26@yahoo.com>
2019-10-15 11:45:42 -0400

    WIP: Adding Resources page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home';
 import Calendar from '../Calendar/Calendar';
+import Resources from '../Resources/Resources';
 import Classes from '../Classes/Classes';
 import Contact from '../Contact/Contact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -10,6 +11,7 @@ import TodayIcon from '@material-ui/icons/Today';
 import HomeIcon from '@material-ui/icons/Home';
 import SchoolIcon from '@material-ui/icons/School';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import './App.scss';
 
 const theme = createMuiTheme({
@@ -30,9 +32,9 @@ class App extends Component {
     const homeDescs = ['Need to Check Office Hours?', 'Checking what classes I am the TA for?', 'Looking for contact information?'];
     const homeLinkTitles = ['View Calendar', 'See Supported Classes', 'Get My Contact Info'];
     const homeLinks = ['/calendar', '/classes', '/contact'];
-    const navTitles = ['Home', 'Calendar', 'Classes', 'Contact'];
-    const navLinks = ['/', '/calendar', '/classes', '/contact'];
-    const navIcons = [<HomeIcon key={0} />, <TodayIcon key={1} />, <SchoolIcon key={3} />, <ContactMailIcon key={4} />];
+    const navTitles = ['Home', 'Calendar', 'Resources', 'Classes', 'Contact'];
+    const navLinks = ['/', '/calendar', '/resources', '/classes', '/contact'];
+    const navIcons = [<HomeIcon key={0} />, <TodayIcon key={1} />, <CloudDownloadIcon key={2} />, <SchoolIcon key={3} />, <ContactMailIcon key={4} />];
     const classNums = ['CSCI 2212 / 6610', 'CSCI 4526 / 6626', 'CSCI 4453 / 6672'];
     const classNames = ['Intermediate C/C++', 'Object Oriented Programming w/ C++', 'Android Development'];
     return (
@@ -46,6 +48,9 @@ class App extends Component {
               </Route>
               <Route exact path='/calendar'>
                 <Calendar />
+              </Route>
+              <Route exact path='/resources'>
+                <Resources />
               </Route>
               <Route exact path='/classes' >
                 <Classes classNames={classNames} classNums={classNums}/>
---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Resources.scss';
+
+class Resources extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Resources</h1>
+    );
+  }
+
+}
+
+export default Resources;
