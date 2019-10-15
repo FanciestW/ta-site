7bfa408d9a9752c13754a98b8d753a1e846e082e
William Lin <wlin26@yahoo.com>
2019-10-15 12:54:40 -0400

    Added grid to center list in middle of page

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Divider, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from '@material-ui/core';
+import { Divider, Grid, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from '@material-ui/core';
 import LinkIcon from '@material-ui/icons/Link';
 import './Resources.scss';
 
@@ -15,29 +15,36 @@ class Resources extends Component {
 
   render() {
     return (
-      <div className='root'>
-        <List component="nav" aria-label="links">
-          <ListSubheader component="div">Intermediate C/C++</ListSubheader>
-          <ListItemLink target='_blank' href='https://unhnewhaven-my.sharepoint.com/:f:/g/personal/wlin2_unh_newhaven_edu/EiDLMHQp92ZKn82kkwNC5oEBTHzMyMQu6q-oJSh_SEIy-Q?e=AD3G14'>
-            <ListItemIcon>
-              <LinkIcon />
-            </ListItemIcon>
-            <ListItemText primary="Bridge Class Lecture Slides" />
-          </ListItemLink>
-          <ListItemLink target='_blank' href='http://www.cplusplus.com/'>
-            <ListItemIcon>
-              <LinkIcon />
-            </ListItemIcon>
-            <ListItemText primary="Applied Introductory C Programming - Textbook Link" />
-          </ListItemLink>
-          <ListItemLink target='_blank' href='http://eliza.newhaven.edu/apc'>
-            <ListItemIcon>
-              <LinkIcon />
-            </ListItemIcon>
-            <ListItemText primary="C++ Reference Site" />
-          </ListItemLink>
-        </List>
-      </div>
+      <Grid container
+        spacing={0}
+        direction='column'
+        alignItems='center'
+        justify='center'
+        style={{minHeight: '100vh'}}>
+        <div className='list'>
+          <List component="nav" aria-label="links">
+            <ListSubheader component="div">Intermediate C/C++</ListSubheader>
+            <ListItemLink target='_blank' href='https://unhnewhaven-my.sharepoint.com/:f:/g/personal/wlin2_unh_newhaven_edu/EiDLMHQp92ZKn82kkwNC5oEBTHzMyMQu6q-oJSh_SEIy-Q?e=AD3G14'>
+              <ListItemIcon>
+                <LinkIcon />
+              </ListItemIcon>
+              <ListItemText primary="Bridge Class Lecture Slides" />
+            </ListItemLink>
+            <ListItemLink target='_blank' href='http://www.cplusplus.com/'>
+              <ListItemIcon>
+                <LinkIcon />
+              </ListItemIcon>
+              <ListItemText primary="Applied Introductory C Programming - Textbook Link" />
+            </ListItemLink>
+            <ListItemLink target='_blank' href='http://eliza.newhaven.edu/apc'>
+              <ListItemIcon>
+                <LinkIcon />
+              </ListItemIcon>
+              <ListItemText primary="C++ Reference Site" />
+            </ListItemLink>
+          </List>
+        </div>
+      </Grid>
     );
   }
 
