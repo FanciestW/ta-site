bb55abacfb74eb066ff0bf56bbe879c55a7b0fd3
William Lin <wlin26@yahoo.com>
2020-02-10 18:37:46 -0500

    Updated supported classes page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -50,8 +50,8 @@ class App extends Component {
     const navTitles = ['Home', 'Calendar', 'Resources', 'Classes', 'Contact'];
     const navLinks = ['/', '/calendar', '/resources', '/classes', '/contact'];
     const navIcons = [<HomeIcon key={0} />, <TodayIcon key={1} />, <CloudDownloadIcon key={2} />, <SchoolIcon key={3} />, <ContactMailIcon key={4} />];
-    const classNums = ['CSCI 2212 / 6610', 'CSCI 4526 / 6626', 'CSCI 4453 / 6672'];
-    const classNames = ['Intermediate C/C++', 'Object Oriented Programming w/ C++', 'Android Development'];
+    const classNums = ['CSCI 2212 / 6610', 'CSCI 4536 / 6636', 'CSCI 3351 / 6651'];
+    const classNames = ['Intermediate C/C++', 'Structure of Programming Languages', 'Script Programming/Python'];
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
