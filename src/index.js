b851eb29a156ee51ae707dc2ffcd78bcb0880fd9
William Lin <wlin26@yahoo.com>
2021-02-02 13:45:16 -0500

    Moved Landing component to be within the App component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import Landing from './components/Landing/Landing';
 import * as serviceWorker from './serviceWorker';
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-88238825-4');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
